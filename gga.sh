a7f38150ce1c3c70086976d8ff0ebff6f79a6c92
wongdjowo <109690021+wongdjowo@users.noreply.github.com>
2022-07-26 08:21:29 +0700

    Update gga.sh

--- gga.sh
+++ gga.sh
@@ -1,4 +1,27 @@
-rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
+rm -rf ngrok ngrok.zip gga.sh > /dev/null 2>&1
 wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok.zip > /dev/null 2>&1
 ./ngrok authtoken 2CF4M8B2jaQWablHDVEcHKGZxE8_2wzZLHAbfhfZGc6CgH25h
+clear
+echo "======================="
+./ngrok tcp --region ap 3388 &>/dev/null &
+echo "===================================="
+echo "Install RDP"
+echo "===================================="
+docker pull danielguerra/ubuntu-xrdp
+clear
+echo "===================================="
+echo "Start RDP"
+echo "===================================="
+echo "===================================="
+echo "Username : ubuntu"
+echo "Password : ubuntu"
+echo "RDP Address:"
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "===================================="
+echo "===================================="
+echo "Don't close this tab to keep RDP running"
+echo "Wait 2 minute to finish the setup then Run using RDP Address"
+echo "===================================="
+echo "===================================="
+docker run --rm -p 3388:3389 danielguerra/ubuntu-xrdp:kali > /dev/null 2>&1
