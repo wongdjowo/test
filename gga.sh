d6020746678126462b1e2718af4101cfd2c2eaf3
wongdjowo <109690021+wongdjowo@users.noreply.github.com>
2022-07-26 08:12:08 +0700

    Create gga.sh

---
+++ gga.sh
@@ -0,0 +1,4 @@
+rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
+wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok.zip > /dev/null 2>&1
+./ngrok authtoken 2CF4M8B2jaQWablHDVEcHKGZxE8_2wzZLHAbfhfZGc6CgH25h
